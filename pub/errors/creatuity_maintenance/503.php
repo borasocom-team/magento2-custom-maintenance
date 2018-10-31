395492e1a90956e078163bbe5cd5648450e26efb
Fabio Brunelli <arioch1984@gmail.com>
2018-10-31 15:22:30 +0100

    removed pub/errors directory

--- pub/errors/creatuity_maintenance/503.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require_once 'processorFactory.php';
-$processorFactory = new \Creatuity\Error\ProcessorFactory;
-$processor = $processorFactory->createProcessor();
-$response = $processor->process();
-$response->sendResponse();
